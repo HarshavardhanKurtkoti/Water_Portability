6d11acddf9f22cc5b00116be141ab1bde7e5ae7c
Harshavardhan <kurtkoti.harsha@gmail.com>
2025-04-14 16:03:52 +0530

    3rd stage

---
+++ src/model_building.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import numpy as np
+import pickle
+import os
+
+from sklearn.ensemble import RandomForestClassifier
+
+train_data = pd.read_csv('data/processed/test_processed.csv')
+
+X_train = train_data.iloc[:,0:-1].values
+y_train = train_data.iloc[:,-1].values
+
+clf = RandomForestClassifier()
+clf.fit(X_train, y_train)
+
+base_dir = os.path.dirname(os.path.abspath(__file__))
+model_dir = os.path.join(base_dir, '../model')
+
+os.makedirs(model_dir, exist_ok=True)
+
+model_path = os.path.join(model_dir, 'model.pkl')
+pickle.dump(clf, open(model_path, 'wb'))
