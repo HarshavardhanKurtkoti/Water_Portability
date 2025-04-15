f4ba64a159227a8dc71bab41970ac6b21d90f2e1
Harshavardhan <kurtkoti.harsha@gmail.com>
2025-04-15 21:24:02 +0530

    main file updated

---
+++ src/main.py
@@ -0,0 +1,41 @@
+from fastapi import FastAPI
+from fastapi.responses import FileResponse
+import pickle
+import pandas as pd
+
+from .data_model import Water 
+
+app = FastAPI(
+    title="Water Potability Prediction API",
+    description="API for predicting water potability using a machine learning model.",
+)
+
+with open("model/model.pkl", "rb") as model_file:
+    model = pickle.load(model_file)
+
+@app.get("/")
+def index():
+    return {"message": "Welcome to the Water Potability Prediction API"}
+
+@app.post("/predict")
+def model_predict(water: Water):
+    sample = pd.DataFrame({
+        'ph' : [water.ph],
+        'Hardness' : [water.Hardness],
+        'Solids' :[water.Solids],
+        'Chloramines' :[water.Chloramines],
+        'Sulfate' : [water.Sulfate],
+        'Conductivity' :[water.Conductivity],
+        'Organic_carbon' :[water.Organic_carbon],
+        'Trihalomethanes' : [water.Trihalomethanes],
+        'Turbidity' :[water.Turbidity]
+    })
+
+    predicted_value = model.predict(sample)[0]
+    
+    if predicted_value == 1:
+        result = "Potable"
+    else:
+        result = "Not Potable"
+        
+    return {"prediction": result}
